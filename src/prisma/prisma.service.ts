855656980a7e83455708a8dd256cdf2698d2ff48
stevesdiary <stephenoyeyemi@gmail.com>
2024-02-08 23:03:54 +0100

    Prisma migration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: 'mysql://stephenoyeyemi:12345@localhost:3306/testproject'
+      },
+    }),
+  }
+}
